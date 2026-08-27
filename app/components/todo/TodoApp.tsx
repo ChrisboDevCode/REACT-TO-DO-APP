0ef4c1397e80caa03a687b89ce54d2903aaff27c
ChrisboDevCode <christian.ruben@outlook.com>
2026-08-26 23:14:22 -0500

    Proyecto TO-DO con react - Next js

---
+++ app/components/todo/TodoApp.tsx
@@ -0,0 +1,30 @@
+export function TodoApp() {
+    return (
+        <div className="px-10">
+            <h1>Lista de Tareas</h1>
+            <div className="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl">
+                <div className="flex">
+                    <div className="shrink-0">
+                    {/* <img
+                        className="h-48 w-full object-cover md:h-full md:w-48"
+                        src="/"
+                        alt="Modern building architecture"
+                    /> */}
+                    </div>
+                    <div className="p-8">
+                    <div className="text-sm font-semibold tracking-wide text-indigo-500 uppercase">Completar tus tareas diarias</div>
+                    {/* <a href="#" className="mt-1 block text-lg leading-tight font-medium text-black hover:underline">
+                        Incredible accommodation for your team
+                    </a>
+                    <p className="mt-2 text-gray-500">
+                        Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of
+                        places to do just that.
+                    </p> */}
+                    </div>
+            </div>
+</div>
+
+        </div>
+        
+    )
+}
---
+++ app/types/todo.ts
@@ -0,0 +1,9 @@
+
+export type TodoStatus = true | false;
+export interface Welcome1 {
+    id:        number;
+    todo:      string;
+    completed: TodoStatus;
+    userId:    number;
+}
+
